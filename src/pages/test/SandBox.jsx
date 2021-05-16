ffb4d6209a9cb57f86a701db37848598c873aa68
Hafiz Joundy Syafie <hafiz@ruangguru.com>
2021-05-16 22:23:10 +0700

    poc useComponentMutation

--- src/pages/test/SandBox.jsx
+++ src/pages/test/SandBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useQuery } from 'react-query';
-import { getUser } from '../../client/rest/apis';
+import { useMutation, useQuery } from 'react-query';
+import { createUser, getUser } from '../../client/rest/apis';
 import { getTextFromErr } from '../../utils/errors';
 
 const defaultErrorComponent = (error, refetch) => (
@@ -34,9 +34,48 @@ const useComponentQuery = (query, component) => {
   return component.main(data);
 };
 
-export default () => useComponentQuery(useQuery('user', getUser, {
-  retry: 0,
-}), {
-  loading: () => <p>Loading</p>,
-  main: (data) => <p>{JSON.stringify(data)}</p>,
-});
+const useComponentMutation = (mutation, component) => {
+  if (mutation.isLoading) {
+    return component.loading();
+  }
+
+  if (mutation.isError) {
+    if (!component.error === undefined) {
+      return component.error(mutation.error);
+    }
+    // return defaultErrorComponent(mutation.error, () => {});
+    return (
+      <p>
+        error:
+        {' '}
+        {getTextFromErr(mutation.error)}
+      </p>
+    );
+  }
+
+  return component.main(mutation.data, mutation.mutate);
+};
+
+export default () => (
+  <>
+    <p>Example using useComponentQuery: </p>
+    {useComponentQuery(useQuery('user', getUser, {
+      retry: 0,
+    }), {
+      loading: () => <p>Loading</p>,
+      main: (data) => <p>{JSON.stringify(data)}</p>,
+    })}
+    <br />
+
+    <p>Example using useComponentMutation</p>
+    {useComponentMutation(useMutation('user-mutation', () => createUser('hafizjoundys@gmail.com')), { // example using mutation
+      loading: () => <p>create user ..</p>,
+      main: (data, mutate) => (
+        <>
+          <p>{JSON.stringify(data)}</p>
+          <button onClick={mutate} type="button">Create User</button>
+        </>
+      ),
+    })}
+  </>
+);
